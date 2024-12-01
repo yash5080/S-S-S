a44792d1e8db3d6e46debf4ed3eb9ef3d5d96b3e
Yaswanth Pedapudi <135613732+yash5080@users.noreply.github.com>
2024-12-01 16:33:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('orderForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Order Placed Successfully! We will contact you soon.');
+});
